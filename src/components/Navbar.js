877007a60976d567a15c57132c8cfe7582cc777d
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-03-03 00:09:02 +0300

    added navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import { Link, NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+      <div className="container-fluid">
+        <Link className="navbar-brand" exact to="/">
+          Todo
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav ms-auto">
+            <NavLink className="nav-link" to="/add-to-do">
+              Add Todo
+            </NavLink>
+            <NavLink className="nav-link" to="/to-do">
+              View Todo
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
