99b15b9e5d1c53e4d82b3d1056660860c52b923b
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-03-03 00:13:39 +0300

    added index.js and app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Homepage from "./components/Homepage";
+import NavBar from "./components/Navbar";
+import Todo from "./components/Todo";
+import TodoForm from "./components/TodoForm";
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/to-do" element={<Todo />} />
+        <Route path="/add-to-do" element={<TodoForm />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
