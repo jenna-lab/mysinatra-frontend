02004b297413146cad1706d7f0c86dd141da12cd
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-03-04 13:04:13 +0300

    added delete functionality

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -9,6 +9,24 @@ const TodoList = () => {
       .then((todos) => setTodos(todos));
   }, []);
 
+  const handleDelete = (id) => {
+    console.log("deleted", id);
+    fetch(`http://localhost:9292/todos/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (response.ok) {
+          setTodos(todos.filter((todo) => todo.id !== id));
+        } else {
+          throw new Error("Something went wrong while deleting the blog.");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    alert("deleted successfully");
+  };
+
   return (
     <div className="container">
       {todos.map((todo, id) => (
@@ -18,7 +36,12 @@ const TodoList = () => {
           <li>{todo.due_date}</li>
           <button className="btn btn-secondary">Edit</button>{" "}
           <button className="btn btn-primary">Save</button>{" "}
-          <button className="btn btn-danger">Delete</button>
+          <button
+            className="btn btn-danger"
+            onClick={() => handleDelete(todo.id)}
+          >
+            Delete
+          </button>
         </ul>
       ))}
     </div>
